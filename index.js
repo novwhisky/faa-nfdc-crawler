0de3e094cc7ae9e16cdb84071dcbc7bc9f0919ac
Nathan Witt <wittnl@gmail.com>
2016-04-02 12:02:50 -0500

    Streaming Airport Data parser

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
-var icao = require('./lib/icao-filtered');
+var fs = require('fs');
+var util = require('util');
 
-console.log(icao);
+var icao = require('./lib/icao-filtered');
+var AirportDataStream = require('./lib/nfdc/AirportDataResultParser');
+
+var apStrm = fs.createReadStream('out.html');
+var airportData = new AirportDataStream();
+
+apStrm.on('open', function() {
+  apStrm.pipe(airportData);
+});
